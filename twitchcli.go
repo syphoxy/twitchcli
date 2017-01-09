007176fc92664e179f431b9084586d816059e40e
Dario Russo <spyroboy@dariorusso.ca>
2017-01-09 10:53:22 -0500

    Change API repo, minor conciseness changes

--- twitchcli.go
+++ twitchcli.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/mrshankly/go-twitch/twitch"
+	"github.com/syphoxy/go-twitch/twitch"
 	"log"
 	"net/http"
 )
 
 func main() {
 	client := twitch.NewClient(&http.Client{})
-	opt := &twitch.ListOptions{
-		Game: "Dota 2",
-	}
 
-	games, err := client.Streams.List(opt)
+	games, err := client.Streams.List(&twitch.ListOptions{
+		Game: "Dota 2",
+	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, s := range games.Streams {
-		fmt.Printf("%6d %s: %s\n", s.Viewers, s.Channel.DisplayName, s.Channel.Status)
+		fmt.Printf("%8d %25s %s\n", s.Viewers, s.Channel.DisplayName, s.Channel.Status)
 	}
 }
