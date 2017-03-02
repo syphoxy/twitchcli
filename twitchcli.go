22f6be23056447cb5c907b04790b474519f9e0da
Dario Russo <spyroboy@dariorusso.ca>
2017-03-01 22:46:27 -0500

    Add game and limit flags

--- twitchcli.go
+++ twitchcli.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/syphoxy/go-twitch/twitch"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	client := twitch.NewClient(&http.Client{})
 
-	games, err := client.Streams.List(&twitch.ListOptions{
-		Game: "Dota 2",
-	})
+	var game string
+	var limit int
+
+	flag.StringVar(&game, "g", "", "Game")
+	flag.IntVar(&limit, "l", 25, "Limit")
+	flag.Parse()
+
+	if limit <= 0 {
+		os.Exit(0)
+	}
+
+	if limit > 100 {
+		fmt.Println("Limit must be a value between 0 and 100")
+		os.Exit(1)
+	}
+
+	options := twitch.ListOptions{}
+	options.Limit = limit
+
+	if game != "" {
+		options.Game = game
+	}
+
+	games, err := client.Streams.List(&options)
 
 	if err != nil {
 		log.Fatal(err)
